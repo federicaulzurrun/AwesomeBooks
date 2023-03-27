557c241e2a2509d7f7103c3b0831c3a11ebc0256
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-27 14:49:09 -0300

    change functions order, add if conditioner for empty form

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,61 +2,58 @@
 const addedBooks = document.querySelector('#book-list');  
 // Empty array for books
 const books = [];
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
 const submit = document.querySelector('#submit');
 
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  /* Push books */
-  books.push({
-    title: title.value,
-    author: author.value,
-  });
-  console.log(books);
+function addBooks() {
+  const title = document.querySelector('#title');
+  const author = document.querySelector('#author');
+  if (title.value !== '' && author.value !== '') {
+    books.push({
+      title: title.value,
+      author: author.value,
+    });
 
-  localStorage.setItem('books', JSON.stringify(books));
-  title.value = ' ';
-  author.value = ' ';
-});
+    localStorage.setItem('books', JSON.stringify(books));
+    title.value = ' ';
+    author.value = ' ';
+  }
+}
 
 // remove a book from books array
 
 function remove(index) {
-    books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(books));
- }
+  books.splice(index, 1);
+  localStorage.setItem('books', JSON.stringify(books));
+}
 
- function print() {
-    addedBooks.innerHTML = ' ';
-    for (let i = 0; i < books.length; i+= 1) {
-    let html = `
+function print() {
+  addedBooks.innerHTML = ' ';
+  for (let i = 0; i < books.length; i += 1) {
+    const html = `
     <div class="newBook">
-      <div class="book-info">
+    <div class="book-info">
         <div class="title">${books[i].title}</div>
         <div class="author">${books[i].author}</div>
         </div>
         <div class="removeBtnCont">
         <button class="removeBook">Remove Book</button>
         </div>
-        </div>`; 
+        </div>`;
     addedBooks.innerHTML += html;
-    }
+  }
 
-    //create Remove button dynamically
-    removeBtn = document.querySelectorAll('.removeBook');
-    for (let i = 0; i < books.length; i +=1) {
-        removeBtn[i].addEventListener('click', () => {
-            remove(i);
-            print();
-        });
-    }
+  // create Remove button dynamically
+  removeBtn = document.querySelectorAll('.removeBook');
+  for (let i = 0; i < books.length; i += 1) {
+    removeBtn[i].addEventListener('click', () => {
+      remove(i);
+      print();
+    });
+  }
+}
 
- }
-
-
-
-
-
-
-
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBooks();
+  print();
+});
