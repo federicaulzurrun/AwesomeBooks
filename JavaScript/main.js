17689592742f50df0911a1fe6bd66fe4e7d21608
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-27 13:23:54 -0300

    add function for books creation

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,2 +1,18 @@
-let books = [];
-console.log(books);
+// Empty array for books
+
+const books = [];
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  /* Push books */
+  books.push({
+    title: title.value,
+    author: author.value,
+  });
+  console.log(books);
+
+  localStorage.setItem('books', JSON.stringify(books));
+});
