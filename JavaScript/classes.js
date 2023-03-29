cf7f9e406b216a584692cae70ad86b8acd0a87e0
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-03-29 13:02:58 -0300

    Add style

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -1,33 +1,39 @@
-  class Books {
+// Create a book Class
+class Books {
   constructor() {
     this.books = [];
+    // Call the HTML elements
     this.title = document.getElementById('title');
-    this.author= document.getElementById('author');
+    this.author = document.getElementById('author');
     this.addButton = document.getElementById('submit');
     this.bookList = document.getElementById('bookList');
 
     this.displayBooks();
 
+    /* add an event listener to call the addbutton function and display the list of books */
     this.addButton.addEventListener('click', (e) => {
       e.preventDefault();
       this.addBooks();
       this.displayBooks();
     });
+
+    /* add an event listener to call the removebutton function and remove the list of books */
     this.bookList.addEventListener('click', (event) => {
       if (event.target.classList.contains('removeBtn')) {
-        const bookIndex = event.target.dataset.bookIndex;
+        const { bookIndex } = event.target.dataset;
         this.removeBook(bookIndex);
       }
     });
   }
 
-  // display books
+  /* Create Methods for the Book Class */
+  // display books. Create li and remove button elements
   displayBooks() {
     this.bookList.innerHTML = '';
     this.books.forEach((book, index) => {
       const li = document.createElement('li');
       li.classList.add('book-infocss');
-      li.innerHTML = `"${book.title}" by ${book.author}`;
+      li.innerHTML = `<p>"${book.title}" by ${book.author}</p>`;
       const removeBtn = document.createElement('button');
       removeBtn.textContent = 'Remove';
       removeBtn.classList.add('removeBtn');
@@ -37,7 +43,7 @@
     });
   }
 
-  // add book method
+  // add book
   addBooks() {
     if (this.title.value !== '' && this.author.value !== '') {
       const title = this.title.value;
@@ -59,8 +65,10 @@
   }
 }
 
+// Creating a instance of Books
 const awesomeBooks = new Books();
 
+// Checking Local Storage
 if (localStorage.getItem('createdBooks')) {
   awesomeBooks.books = JSON.parse(localStorage.getItem('createdBooks'));
   awesomeBooks.displayBooks();
