f343c55c1c1be8ae6640c0138aae209be28c8e3a
Maedea9 <melaniecascante.j@gmail.com>
2023-03-28 22:57:47 -0500

    Fix JavaScript code

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -1,18 +1,21 @@
-class Book {
+  class Books {
   constructor() {
     this.books = [];
     this.title = document.getElementById('title');
-    this.author = document.getElementById('author');
-    this.addBook = document.getElementById('submit');
+    this.author= document.getElementById('author');
+    this.addButton = document.getElementById('submit');
     this.bookList = document.getElementById('bookList');
+
     this.displayBooks();
-    this.addBook.addEventListener('submit', (e) => {
+
+    this.addButton.addEventListener('click', (e) => {
       e.preventDefault();
       this.addBooks();
+      this.displayBooks();
     });
-    this.bookList.addEventListener('click', (e) => {
-      if (e.target.classList.contains('removeBtn')) {
-        const bookIndex = e.target.dataset.BookIndex;
+    this.bookList.addEventListener('click', (event) => {
+      if (event.target.classList.contains('removeBtn')) {
+        const bookIndex = event.target.dataset.bookIndex;
         this.removeBook(bookIndex);
       }
     });
@@ -23,7 +26,7 @@ class Book {
     this.bookList.innerHTML = '';
     this.books.forEach((book, index) => {
       const li = document.createElement('li');
-      li.classList.add('book-info');
+      li.classList.add('book-infocss');
       li.innerHTML = `"${book.title}" by ${book.author}`;
       const removeBtn = document.createElement('button');
       removeBtn.textContent = 'Remove';
@@ -34,11 +37,12 @@ class Book {
     });
   }
 
-  // add book
+  // add book method
   addBooks() {
     if (this.title.value !== '' && this.author.value !== '') {
       const title = this.title.value;
       const author = this.author.value;
+
       this.books.push({ title, author });
       // save book to localstorage separatedly
       localStorage.setItem('createdBooks', JSON.stringify(this.books));
@@ -47,7 +51,7 @@ class Book {
     }
   }
 
-  // remove book
+  // remove book method
   removeBook(bookIndex) {
     this.books.splice(bookIndex, 1);
     localStorage.setItem('createdBooks', JSON.stringify(this.books));
@@ -55,7 +59,7 @@ class Book {
   }
 }
 
-const awesomeBooks = new Book();
+const awesomeBooks = new Books();
 
 if (localStorage.getItem('createdBooks')) {
   awesomeBooks.books = JSON.parse(localStorage.getItem('createdBooks'));
